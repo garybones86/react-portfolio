9c4d862c11af91d2d22a1e7e4cd8d56a7b57b94f
gary bowen <garybowen@garys-Air.lan>
2023-02-12 19:43:26 -0500

    fixed pc version of portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import boxing from "../assets/portfolio/boxing.png";
 import pet from "../assets/portfolio/pet-lyfe.png";
 import shill from "../assets/portfolio/shill.png";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+
 
 const Portfolio = () => {
   const portfolios = [
@@ -13,18 +11,21 @@ const Portfolio = () => {
       src: boxing,
       link: 'https://bug-free-funicular-odew-git-main-dfff.vercel.app/' ,
       repo:  'https://github.com/garybones86/react-portfolio ',
+      description:" Bear Boxing, where warrior get their gear.Ecommerce site for the boxing enthusiast! Built with Next.js for the front-end, Sanity.io headless  CMS for the back-end, Stripe intregration for the payment processing to create a full fledge ecommerce experience.  " ,
     },
     {
       id: 2,
       src: pet,
       link: 'https://pet-lyfe.netlify.app/' ,
       repo:  'https://github.com/Brandon-Alvarez-03/pet-style-mern',
+      description1:"Ecommerce site for stylish pet products! Built with React.js for the front-end, Express.js, MongoDB, and Restful APIs for the back-end, to create a user-friendly and efficient shopping experience. Whether you're looking to spoil your furry friend or simply browse our selection" ,
     },
     {
       id: 3,
       src: shill,
       link: 'https://shill-social.netlify.app/' ,
       repo:  'https://github.com/Brandon-Alvarez-03/pet-style-mern',
+      description2:" Shill, a socoal media platform for the finacial services.This app has   live crytpo updates, bussiness news api  along with the traditional social platform  capabilities( like post, make a post ect. )to create a user-friendly experience. "
     },
    
   ];
@@ -43,13 +44,16 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, link, repo }) => (
+          {portfolios.map(({ id, src, link, repo,description,description1 ,description2}) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt="projects"
                 className="rounded-md duration-200 hover:scale-105"
               />
+              <div> {description} </div>
+              <div> { description1} </div>
+              <div> { description2} </div>
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(link, '_blank')}>
                   Demo
