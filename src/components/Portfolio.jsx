988ddb395da4c6dbe773831e14e56108f2f9aa80
gary bowen <garybowen@garys-Air.lan>
2023-02-12 20:08:28 -0500

    fixed pc version of portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -55,10 +55,21 @@ const Portfolio = () => {
               <div> { description1} </div>
               <div> { description2} </div>
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(link, '_blank')}>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105
+                
+                mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700
+                
+                " onClick={ () => window.open(link, '_blank')}>
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(repo, '_blank')}>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 
+                
+                mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700
+                      
+                      
+                      
+                      
+                      " onClick={ () => window.open(repo, '_blank')}>
                   GitHub
                 </button>
               </div>
